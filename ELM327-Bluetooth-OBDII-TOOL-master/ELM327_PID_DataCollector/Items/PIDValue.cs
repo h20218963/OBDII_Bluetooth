652abc47f1ba66d0bfed283a42fb7ae55486c573
h20218963 <146180370+h20218963@users.noreply.github.com>
2023-10-03 08:37:15 +0530

    Create PIDValue.cs

---
+++ ELM327-Bluetooth-OBDII-TOOL-master/ELM327_PID_DataCollector/Items/PIDValue.cs
@@ -0,0 +1,23 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELM327_PID_DataCollector.Items
+{
+    public class PIDvalue
+    {
+        public int PIDdec { get; set; }
+        public string PIDhex { get; set; }
+        public string Name { get; set; }
+        public int? BitStart { get; set; }
+        public int? BitLength { get; set; }
+        public object? Scale { get; set; }
+        public object? Offset { get; set; }
+        public object? Min { get; set; }
+        public object? Max { get; set; }
+        public string? Unit { get; set; }
+    }
+}
